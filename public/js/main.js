28ee040b0d3c920816fc39595e4dfe5a572aef13
I am naveen Gullipalli <naveengullipalli317@gmail.com>
2021-10-09 09:20:05 +0200

    Chatbox with country option

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
-const roomName = document.getElementById('room-name');
+const roomName = document.getElementById('country-name');
 const userList = document.getElementById('users');
 
 // Get username and room from URL
